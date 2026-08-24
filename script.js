4da17ea37cb418828a4f368f2754517079559867
colindeurlington <c.deurlington@gmail.com>
2026-08-23 21:30:15 -0400

    Fix button URL and update styles in script.js
    Updated button styles and fixed URL syntax in research section.

--- script.js
+++ script.js
@@ -51,8 +51,8 @@ work:`
 research:`
 <div style="display:flex; justify-content:center; align-items:center;">
         <button 
-            onclick="window.location.href = https://escholarship.org/uc/item/4c3845t1#main" 
-            style="width: 175px; height: 50px; text-align:center; font-size:30px; background-color:#203b51; button-hover:#e5e4e2; color:white; padding:5px;
+            onclick="window.location.href = 'https://escholarship.org/uc/item/4c3845t1#main'" 
+            style="width: 175px; height: 50px; text-align:center; font-size:30px; background-color:#203b51; button-hover:#e5e4e2; color:white; padding:25px;
       border:3px solid #ccc;
       border-radius: 5px;">
                 Dissertation
